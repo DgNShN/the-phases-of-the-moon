4178e8cb54903ac93cda1f337081c9d02dfde32b
DGNSHN <33982400+DgNShN@users.noreply.github.com>
2023-02-05 02:20:46 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,80 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . # # .
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . # # #
+        . . # # #
+        . . # # #
+        . . # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        . # # # #
+        . # # # #
+        . # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        . # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . . .
+        # # . . .
+        # # . . .
+        # # . . .
+        . # . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # . .
+        # # # . .
+        # # # . .
+        # # # . .
+        . # # . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        # # # # .
+        # # # # .
+        # # # # .
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        # # # # #
+        # # # # #
+        . # # # .
+        `)
+})
